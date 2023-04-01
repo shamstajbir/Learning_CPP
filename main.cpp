dc22ca952cd262bd1dc3760296775ae480e36f89
shamstajbir <25tajbir@gmail.com>
2023-04-02 03:16:01 +0530

    new code for  value for x and y

--- main.cpp
+++ main.cpp
@@ -1,29 +1,57 @@
+//5.3
 #include <iostream>
 using namespace std;
-class parent{
+class alpha {
 public:
+    alpha(int a) {
+        cout << "Constructing alpha class." << endl;
+        x = a;
+    }
+    void printX() {
+        cout << "Value of x in alpha class: " << x << endl;
+    }
+private:
     int x;
-    void pfx(){
-        cout<<"enter the value of x";
-        cin>>x;
-    }
 };
-class child : public parent {
+
+class beta {
 public:
-    int y;
-    void cfx() {
-        cout << "enter the value of y";
-        cin >> y;
+    beta(float b) {
+        cout << "Constructing beta class." << endl;
+        y = b;
     }
-    void display() const {
-        cout <<x + y;
+    void printY() {
+        cout << "Value of y in beta class: " << y << endl;
     }
+private:
+    float y;
+};
+
+class gamma : public alpha, public beta {
+public:
+    gamma(int a, float b, int c, float d) : alpha(a), beta(b) {
+        cout << "Constructing gamma class." << endl;
+        m = c;
+        n = d;
+    }
+    void printM() {
+        cout << "Value of m in gamma class: " << m << endl;
+    }
+    void printN() {
+        cout << "Value of n in gamma class: " << n << endl;
+    }
+private:
+    int m;
+    float n;
 };
 
 int main() {
-    child obj{};
-    obj.pfx();
-    obj.cfx();
-    obj.display();
+    gamma obj(1, 2.5, 3, 4.5);
+
+    obj.printX();
+    obj.printY();
+    obj.printM();
+    obj.printN();
+
     return 0;
 }
