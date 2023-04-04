a4add503b9a9c138056d723a9277be66c51bdc83
shamstajbir <25tajbir@gmail.com>
2023-04-04 22:39:36 +0530

    5.2 Create a class student having student uid and getnumber(), putnumber() as member functions to get the values and display it. Derive a class test having marks in different subjects and getmarks() and putmarks() as member functions to get and display the values.Derive another classsportsfrom student class having sportsscore and getscore(), putscore() as member functionsto get and display the values. Derive a class result from test and sports class and define a function display() to calculate total marks. Implement it with the object ofresult class. If it gives any error, resolve it by adding the required functionality.

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,43 @@
-//
-// Created by Laptop Point on 3/26/2023.
-//
-
-#include "hello.h"
+#include <iostream>
+using namespace std;
+class Student
+{
+public:
+    int uid;
+    string name;
+    void in()
+    { cout << "Enter uid: ";
+        cin >> uid;
+        cout << "Enter name: ";
+        cin >> name; } };
+class subjects : virtual public Student
+{
+public:
+    float m1;
+    void inn()
+    { cout << "Enter marks in 5 subjects(out of 500): ";
+        cin >> m1; } };
+class sports : virtual public Student
+{
+public:
+    int m2;
+    void innn()
+    {cout << "Enter marks in sports: ";
+        cin >> m2; }};
+class result : public subjects, public sports
+{
+public:
+    void out()
+    { cout << endl
+           << "uid: " << uid << endl
+           << "Name: " << name << endl
+           << "Academic Marks: " << m1/5 << endl
+           << "Sports marks: " << m2 << endl
+           << "Final Result(out of 100): " << float(((m1/5) + m2)) / 2.00; }};
+int main()
+{ result a;
+    a.in();
+    a.inn();
+    a.innn();
+    a.out();
+}
